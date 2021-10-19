139a61e90e0c97958ae801c636ab8e7c2c095976
afcagua-ptv <89595950+afcagua-ptv@users.noreply.github.com>
2021-10-19 16:45:45 -0500

    Delete getParameters.ts

--- src/helpers/requests/getParameters.ts
+++
@@ -1,8 +0,0 @@
-export const getUrlParameterByKey = (url:string, paramKey: string):string => {
-    paramKey = paramKey.replace(/[\[\]]/g, '\\$&');
-    const regex = new RegExp('[?&]' + paramKey + '(=([^&#]*)|&|#|$)'),
-        results = regex.exec(url);
-    if (!results) return '';
-    if (!results[2]) return '';
-    return decodeURIComponent(results[2].replace(/\+/g, ' '));
-}
